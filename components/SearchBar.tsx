93c0fd04ca071426b472d01352b9779dbec7f94f
Riya Torgal <riyatorgal@gmail.com>
2024-01-03 19:05:25 +0530

    small fix

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -50,7 +50,7 @@ const SearchBar = () => {
         <div className='searchbar__item'>
             <SearchManufacturer 
             manufacturer = {manufacturer}
-            setManufacturer = {setManufacturer}
+            setManuFacturer = {setManufacturer}
             />
             <SearchButton otherClasses="sm:hidden"/>
         </div>
